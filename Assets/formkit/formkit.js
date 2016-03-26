5f530d96470fe2cbd0f5684c21c1134b8cd5efa5
c9s <yoanlin93@gmail.com>
2016-03-26 17:11:23 +0900

    remove console.deubg formkit

--- Assets/formkit/formkit.js
+++ Assets/formkit/formkit.js
@@ -1,40 +1,29 @@
 /*
 $(document.body).ready(function() {
-    FormKit.install();
-    FormKit.initialize(document.body);
+  FormKit.install();
+  FormKit.initialize(document.body);
 });
 
 Inside Ajax Region:
 
-    $(document.body).ready(function() {
-        FormKit.initialize( div element );
-    });
-
+  $(document.body).ready(function() {
+    FormKit.initialize( div element );
+  });
 */
 var FormKit = {
-    register: function(initHandler, installHandler) { 
-        if (window.console) {
-          console.debug('formkit.register');
-        }
-        $(FormKit).bind('formkit.initialize',initHandler);
-        if (installHandler) {
-            $(this).bind('formkit.install',installHandler);
-        }
-    },
-    initialize: function(scopeEl) {
-        if (!scopeEl) {
-            scopeEl = document.body;
-        }
-        if (window.console) {
-          console.debug('formkit.initialize');
-        }
-        jQuery(FormKit).trigger('formkit.initialize',[scopeEl]);
-    },
-    install: function() {
-        if (window.console) {
-          console.debug('formkit.install');
-        }
-        $(FormKit).trigger('formkit.install');
+  register: function(initHandler, installHandler) { 
+    $(FormKit).bind('formkit.initialize',initHandler);
+    if (installHandler) {
+      $(this).bind('formkit.install',installHandler);
     }
+  },
+  initialize: function(scopeEl) {
+    if (!scopeEl) {
+      scopeEl = document.body;
+    }
+    jQuery(FormKit).trigger('formkit.initialize',[scopeEl]);
+  },
+  install: function() {
+    $(FormKit).trigger('formkit.install');
+  }
 };
-
