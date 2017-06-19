1da7e8e21209cbbe46d457fa79ccdfbe5cdb7ed6
c9s <yoanlin93@gmail.com>
2017-06-19 21:16:15 +0800

    run php psr fix

--- Action/Uploader.php
+++ Action/Uploader.php
@@ -1,14 +1,12 @@
 <?php
 namespace CoreBundle\Action;
 
-
 use Exception;
 use Phifty\FileUtils;
 
 use ActionKit\Storage\FileRenameMethods;
 
-
-// Upload Header is like:
+// The Upload Header is like:
 //
 //        (
 //            [HOST] => phifty.local
@@ -43,21 +41,22 @@ class Uploader
 
         $this->content = $this->decodeInput();
 
-        if ( function_exists('getallheaders') ) {
-            if ( $headers = getallheaders() ) {
+        if (function_exists('getallheaders')) {
+            if ($headers = getallheaders()) {
                 $this->headers = getallheaders();
             }
         }
-        if ( $this->headers )
+        if ($this->headers) {
             $this->headers = array_change_key_case($this->headers, CASE_UPPER);
+        }
     }
 
     public function foundUpload()
     {
-        if ( count($_FILES) > 0 ) {
+        if (count($_FILES) > 0) {
             return true;
         }
-        if ( $this->content ) {
+        if ($this->content) {
             return true;
         }
         return false;
@@ -70,39 +69,39 @@ class Uploader
 
     public function getFileName()
     {
-        if ( isset($_SERVER['HTTP_X_UPLOAD_FILENAME']) ) {
+        if (isset($_SERVER['HTTP_X_UPLOAD_FILENAME'])) {
             return urldecode($_SERVER['HTTP_X_UPLOAD_FILENAME']);
         }
-        if ( isset( $this->headers[ 'X-UPLOAD-FILENAME' ] ) ) {
+        if (isset($this->headers[ 'X-UPLOAD-FILENAME' ])) {
             return urldecode($this->headers[ 'X-UPLOAD-FILENAME' ]);
         }
-        if ( isset( $_FILES[$this->field]['name'] ) ) {
+        if (isset($_FILES[$this->field]['name'])) {
             return $_FILES[$this->field]['name'];
         }
     }
 
     public function getFileType()
     {
-        if ( isset($_SERVER['HTTP_X_UPLOAD_TYPE']) ) {
+        if (isset($_SERVER['HTTP_X_UPLOAD_TYPE'])) {
             return $_SERVER['HTTP_X_UPLOAD_TYPE'];
         }
-        if ( isset($this->headers[ 'X-UPLOAD-TYPE' ]) ) {
+        if (isset($this->headers[ 'X-UPLOAD-TYPE' ])) {
             return $this->headers[ 'X-UPLOAD-TYPE' ];
         }
-        if ( isset( $_FILES[$this->field]['type'] ) ) {
+        if (isset($_FILES[$this->field]['type'])) {
             return $_FILES[$this->field]['type'];
         }
     }
 
     public function getFileSize()
     {
-        if ( isset($_SERVER['HTTP_X_UPLOAD_SIZE']) ) {
+        if (isset($_SERVER['HTTP_X_UPLOAD_SIZE'])) {
             return $_SERVER['HTTP_X_UPLOAD_SIZE'];
         }
-        if ( isset($this->headers[ 'X-UPLOAD-SIZE' ]) ) {
+        if (isset($this->headers[ 'X-UPLOAD-SIZE' ])) {
             return $this->headers[ 'X-UPLOAD-SIZE' ];
         }
-        if ( isset( $_FILES[$this->field]['size'] ) ) {
+        if (isset($_FILES[$this->field]['size'])) {
             return $_FILES[$this->field]['size'];
         }
     }
@@ -112,7 +111,7 @@ class Uploader
         return $this->headers;
     }
 
-    public function setUploadDir( $dir )
+    public function setUploadDir($dir)
     {
         $this->uploadDir = $dir;
     }
@@ -121,18 +120,18 @@ class Uploader
     {
         $content = file_get_contents('php://input');
         if (isset($_GET['base64'])) {
-            $content = base64_decode( $content );
+            $content = base64_decode($content);
         }
         return $content;
     }
 
     public function hasFile()
     {
-        if ( count($_FILES) > 0 ) {
+        if (count($_FILES) > 0) {
             return true;
         }
 
-        if ( $this->content ) {
+        if ($this->content) {
             return true;
         }
         return false;
@@ -141,41 +140,40 @@ class Uploader
     public function move($newFileName = null)
     {
         if ($this->supportSendAsBinary()) {
-            if ( ! isset($_FILES[$this->field]['name'] )) {
-                throw new Exception( "File field '{$this->field}': name is empty");
+            if (! isset($_FILES[$this->field]['name'])) {
+                throw new Exception("File field '{$this->field}': name is empty");
             }
 
             if ($err = $_FILES[$this->field]['error']) {
-                throw new Exception( "File field {$this->field} error: $err");
+                throw new Exception("File field {$this->field} error: $err");
             }
 
-            /* process with $_FILES */
-            // $_FILES['upload']['tmp_name'];
+            /** process with $_FILES **/
+
+            // TODO: prepend date string
             $filename = $newFileName ? $newFileName : $_FILES[$this->field]['name'];
             $tmpName = $_FILES['upload']['tmp_name'];
             $path = $this->uploadDir . DIRECTORY_SEPARATOR . $filename;
             $path = FileRenameMethods::md5ize($path, $tmpName);
-            if (move_uploaded_file($tmpName, $path ) === false) {
+            if (move_uploaded_file($tmpName, $path) === false) {
                 return false;
             }
             return $path;
-
         } else {
-
-            if ( ! $this->content ) {
+            if (! $this->content) {
                 throw new Exception('No file content to upload');
             }
-            if ( ! $newFileName ) {
+            if (! $newFileName) {
                 $newFileName = $this->getFileName();
             }
-            if ( ! $newFileName ) {
+            if (! $newFileName) {
                 // print_r($_FILES);
                 // print_r($_POST);
                 throw new Exception("filename is not defined in request, please check your HTTP header.");
             }
             $path = $this->uploadDir . DIRECTORY_SEPARATOR . $newFileName;
             $path = FileUtils::filename_increase($path);
-            if ( file_put_contents( $path , $this->content ) === false ) {
+            if (file_put_contents($path, $this->content) === false) {
                 return false;
             }
             return $path;
